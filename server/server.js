e5c795d2b15f7a5fe7e4835679788a677b99b0be
oliver wainwright <owainwright@malicom.com>
2019-04-01 00:08:36 +0000

    initial commit to api repo

---
+++ server/server.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+
+'use strict'
+
+const loopback = require('loopback')
+const boot = require('loopback-boot')
+
+const app = loopback()
+module.exports = app
+
+app.start = function appStart () {
+  // start the web server
+  return app.listen(() => {
+    app.emit('started')
+    const baseUrl = app.get('url').replace(/\/$/, '')
+    console.log('---\n')
+    console.log('Web server listening at: %s', baseUrl)
+    if (app.get('loopback-component-explorer')) {
+      const explorerPath = app.get('loopback-component-explorer').mountPath
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath)
+    }
+  })
+}
+
+const bootOptions = {
+  appRootDir: __dirname,
+  bootDirs: [`${__dirname}/boot/migrations`],
+}
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, bootOptions, (err) => {
+  if (err) throw err
+
+  // start the server if `$ node server.js`
+  if (require.main === module) app.start()
+})
